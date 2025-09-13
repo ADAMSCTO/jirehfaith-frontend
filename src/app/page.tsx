81c0b2c62661790ceec9724824afc716738d042a
ADAMSCTO <adamscto@gmail.com>
2025-09-12 20:57:54 -0700

    Final fix: Removed extra closing parenthesis in onClick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -303,7 +303,6 @@ export default function Home() {
                       avoid: getRecentAvoid(mapToneVariant(emotion)),
                     },
                   })
-
                 }
               >
                 {compose.isPending ? (
